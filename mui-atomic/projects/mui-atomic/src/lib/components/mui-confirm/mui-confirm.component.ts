b879e4d67477419c9b0c74c223d033a55e08339d
simonenkof <simonenko.f.a@gmail.com>
2025-09-19 17:03:48 +0300

    fix/build: remove unused props value

--- mui-atomic/projects/mui-atomic/src/lib/components/mui-confirm/mui-confirm.component.ts
+++ mui-atomic/projects/mui-atomic/src/lib/components/mui-confirm/mui-confirm.component.ts
@@ -1,14 +1,12 @@
-import {Component, Injector} from '@angular/core';
-import {MuiButtonComponent} from '../mui-button/mui-button.component';
-import {MuiDialogComponent} from '../mui-dialog/mui-dialog.component';
+import { Component, Injector } from '@angular/core';
+import { MuiButtonComponent } from '../mui-button/mui-button.component';
+import { MuiDialogComponent } from '../mui-dialog/mui-dialog.component';
 
 @Component({
   selector: 'mui-confirm',
   standalone: true,
   templateUrl: './mui-confirm.component.html',
-  imports: [
-    MuiButtonComponent
-  ],
+  imports: [MuiButtonComponent],
   styleUrl: './mui-confirm.component.scss'
 })
 export class MuiConfirmComponent {
@@ -16,8 +14,8 @@ export class MuiConfirmComponent {
   text?: string;
   acceptButtonText: string = 'Да';
   rejectButtonText: string = 'Нет';
-  acceptButtonStyle: 'cta' | 'primary' | 'secondary' | 'outline' | 'alert' = 'primary';
-  rejectButtonStyle: 'cta' | 'primary' | 'secondary' | 'outline' | 'alert' = 'outline';
+  acceptButtonStyle: 'primary' | 'secondary' | 'outline' | 'alert' = 'primary';
+  rejectButtonStyle: 'primary' | 'secondary' | 'outline' | 'alert' = 'outline';
 
   constructor(private _injector: Injector) {
     this._dialogParent = this._injector.get<MuiDialogComponent<any>>(MuiDialogComponent);
